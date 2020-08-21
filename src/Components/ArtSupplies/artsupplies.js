0f0ead51012ef274f1c6e7b1bb79c8dd4b9e3744
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-08-21 01:47:15 +0100

    Created Component

--- src/Components/ArtSupplies/artsupplies.js
+++ src/Components/ArtSupplies/artsupplies.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 // IMPORTING REACT ROUTER DOM
 
-export default class artist extends  Component{
+export default class supplies extends  Component{
 
     render(){
         return(
             <div  className="main">
-                <h1>Artist Page</h1>
+                <h1>Supplies Page</h1>
             </div>
         )
     }
