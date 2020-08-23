77f8548dfa8bd50084ea422359c9cf8662635e4e
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-08-23 19:16:20 +0100

    added search functionality for artist page

--- src/Components/artist/artist.js
+++ src/Components/artist/artist.js
@@ -19,19 +19,30 @@ class artist extends  Component{
 
         this.state = {
             artistData: artistData,
+            input: '',
         }
     }
 
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({input: e.target.value});
+    }
+
     render(){
+        const {artistData, input} = this.state;
+        const filteredArtists = artistData.filter(artist => {
+            return artist.name.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(filteredArtists, input);
         return(
             <div className="main">
                 <div className='artist content'>
                     <h2>Artists</h2>
                     <div className='page-description'>
-                        <Input inputName='search' placeholder='Search'/>
+                        <Input inputName='search' placeholder='Search' onChange={this.handleChange}/>
                         <p>Discover top artists and emerging talents in the industry an view their collections.</p>
                     </div>
-                    <ArtistCollection artists={this.state.artistData}/>
+                    <ArtistCollection artists={filteredArtists}/>
                 </div>
             </div>
         )
