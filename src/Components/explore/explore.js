e07d5ca8f1b4f2b23a5dfceb9512e1587e7b3407
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-08-24 02:26:27 +0100

    Update explore.js

--- src/Components/explore/explore.js
+++ src/Components/explore/explore.js
@@ -20,32 +20,129 @@ export default class explore extends  Component{
                <div className="explore-images-body">
                    <div className="explore-content">
                        <img src="images/img-one.png" alt="" className="explore-img"></img>
+                       <div className="explore-content-body"> 
+                           <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                           <div className="explore-content-text">
+                               <div className="explore-content-text-title">
+                               <h5>Nigerian Art Festival 2020</h5>
+                               <span className="tag">Exhibition</span>
+                               </div>
+                               <p>The event took place on the 25th of August, 2020. It was an event filled...</p>
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
                    <div className="explore-content">
-                       <img src="images/img-two.png" alt="" className="explore-img" ></img>
+                       <img src="images/img-four.png" alt="" className="explore-img" ></img>
+                       <div className="explore-content-body">
+                       <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                              <div className="explore-content-text">
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
                    <div className="explore-content">
-                       <img src="images/img-three.png" alt=""  className="explore-img"></img>
+                       <img src="images/img-seven.png" alt=""  className="explore-img"></img>
+                       <div className="explore-content-body">
+                       <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                           <div className="explore-content-text">
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
                </div>
                <div className="explore-images-body">
                    <div className="explore-content">
-                       <img src="images/img-four.png" alt=""  className="explore-img"></img>
+                       <img src="images/img-two.png" alt=""  className="explore-img"></img>
+                       <div className="explore-content-body">
+                       <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                           <div className="explore-content-text">
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
                    <div className="explore-content">
                        <img src="images/img-five.png" alt="" className="explore-img"></img>
+                       <div className="explore-content-body">
+                       <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                           <div className="explore-content-text">
+                               <div className="explore-content-text-title">
+                               <h5>The Face man</h5>
+                               </div>
+                               <p>Charcoal</p>
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
                    <div className="explore-content">
-                       <img src="images/img-six.png" alt=""  className="explore-img"></img>
+                       <img src="images/img-eight.png" alt=""  className="explore-img" height="200px"></img>
+                       <div className="explore-content-body">
+                       <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                           <div className="explore-content-text">
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
                </div>
                
                <div className="explore-images-body">
                <div className="explore-content">
-                       <img src="images/img-seven.png" alt=""  className="explore-img"></img>
+                       <img src="images/img-three.png" alt=""  className="explore-img"></img>
+                       <div className="explore-content-body">
+                       <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                           <div className="explore-content-text">
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
                    <div className="explore-content">
-                       <img src="images/img-eight.png" alt=""  className="explore-img"></img>
+                       <img src="images/img-six.png" alt=""  className="explore-img"></img>
+                       <div className="explore-content-body">
+                       <div className="explore-user-detail">
+                               <img src="images/user-img.png"></img><span className="explore-username">realgallery</span> <button className="follow">Follow</button>
+                           </div>
+                           <div className="explore-content-text">
+                               <div className="user-reaction row container">
+                                   <div><img src="images/comment.svg"></img></div>
+                                   <div><img src="images/like.svg"></img></div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
              
                    
