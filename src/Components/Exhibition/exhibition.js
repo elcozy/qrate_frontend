d397253dfe89e6dd59f035ef5a89523c1fe9a52e
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-08-29 01:53:47 +0100

    ..

--- src/Components/Exhibition/exhibition.js
+++ src/Components/Exhibition/exhibition.js
@@ -6,14 +6,26 @@ export default class exhibition extends Component{
     render(){
         return(
             <div className="main content">
-                <div className="hero jumbotron-fluid">
-                       <div className="container">
-                             <h1 className="display-3">Fluid jumbo heading</h1>
-                               <p className="lead">Jumbo helper text</p>
-                               <hr className="my-2" />
-                               <p>More info</p>
-                        </div>
-                    </div>
+                <div>
+                 <div className="ex-bg container-lg" style={{backgroundImage: "url(images/banner.png)"}}>
+        <div className="filter"></div>
+        <div className="filter-text  container-md">
+        <h4 className="text-white">Art De Supreme 2020</h4>
+        <div className="attend">
+          <span className="viewers"><img src="images/contact.svg" alt=" "/> 13,458 viewers </span>
+          <span className="location"><img src="images/location.svg"alt="" /> Nike Art Gallery, Lekki, Lagos </span>
+        </div>
+        <div className="live">
+          <img src="images/live.svg" alt=""/>  Live
+        </div>
+        <div className=" button row">
+          <div className="col-md"><button className="btn watch">Watch Live Video</button></div>
+          <div className="col-md"><button className="btn watch-later">Watch In 3D</button></div>
+        </div>
+        </div>
+        </div>
+        </div>
+      
    
             </div>
         )
